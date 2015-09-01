2bbeca126f8fec0c541080a083bcb1e7250b694e
Joscha Holzhaeuer <j.holzhaeuerapps@outlook.com>
2015-09-01 18:04:39 +0200

    added scroll top button

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -7,6 +7,14 @@ JAVASCRIPT: MAIN
 
 $(document).ready(function() {
 
+    var offset = 300,
+        //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+        offset_opacity = 1200,
+        //duration of the top scrolling animation (in ms)
+        scroll_top_duration = 600,
+        //grab the "back to top" link
+        $back_to_top = $('.cd-top');
+
     var contentSections    = $('.article-section'),
         navigationItems    = $('.sidenav-toplink'),
         navigationItemsSec = $('.sidenav-toplink a'),
@@ -22,6 +30,7 @@ $(document).ready(function() {
     $(window).on('scroll', function(){
         updateNavigation();
         scrollNavigation();
+        backToTop();
     });
 
     //smooth scroll to the section
@@ -59,11 +68,33 @@ $(document).ready(function() {
     }
 
     function smoothScroll(target) {
-        $('body,html').animate(
-            {'scrollTop':target.offset().top - 100},
-            600
+        $('body,html').animate({
+            'scrollTop': target.offset().top - 100},
+            scroll_top_duration
         );
     }
+
+    function backToTop() {
+        if ( $(this).scrollTop() > offset ) {
+            $back_to_top.addClass('cd-is-visible');
+
+        } else {
+            $back_to_top.removeClass('cd-is-visible cd-fade-out');
+        }
+
+        if ( $(this).scrollTop() > offset_opacity ) {
+            $back_to_top.addClass('cd-fade-out');
+        }
+    }
+
+    //smooth scroll to top
+    $back_to_top.on('click', function(event){
+        event.preventDefault();
+        $('body,html').animate({
+            scrollTop: 0 ,
+            }, scroll_top_duration
+        );
+    });
 
 }); // end document.ready
 // Avoid `console` errors in browsers that lack a console.
